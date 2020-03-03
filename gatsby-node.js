2974ca4584cc5690b9e0ba42bc8259fc7c0905a2
Ward Peeters <ward@coding-tech.com>
2020-03-03 13:38:18 +0100

    fix: only use generated chunks

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,16 +1,29 @@
 const execa = require("execa");
 
 exports.onPostBuild = async ({ reporter }) => {
+  let stats;
+  try {
+    stats = require("./public/webpack.stats.json");
+  } catch (err) {
+    reporter.warnt('Cannot get webpack stats, try running "gatsby clean"');
+    return;
+  }
+
+  const chunks = [];
+  for (const key in stats.assetsByChunkName) {
+    chunks.push(...stats.assetsByChunkName[key]);
+  }
+
   reporter.info(`generating source-map files..`);
   await execa("yarn", [
     "source-map-explorer",
-    "public/**/*.js",
+    `public/{${chunks.join(",")}}`,
     "--html",
     "public/_analyze.html"
   ]);
   await execa("yarn", [
     "source-map-explorer",
-    "public/**/*.js",
+    `public/{${chunks.join(",")}}`,
     "--gzip",
     "--html",
     "public/_analyze.gz.html"
